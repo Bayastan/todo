4d3cf24701c50eba7b593440b67974a31ed3210b
Bayastan <bayastanbel@gmail.com>
2021-01-25 00:51:12 +0600

    init

--- todo/urls.py
+++ todo/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path("test/", test, name="test"),
     path("test2/", second),
     path("test3/", third)
-    + static(settings.STATIC_URL, document.root=settings.STATIC_ROOT) \
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
