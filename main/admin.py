21cc67b209e08015638b379dbb754999e9a76ff4
Bayastan <bayastanbel@gmail.com>
2021-01-26 02:14:32 +0600

    added books paga in templates

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import ToDo
+from .models import Books
 
 admin.site.register(ToDo)
+admin.site.register(Books)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .models import ToDo
+from .models import Books
 
 def homepage(request):
     return render(request, "index.html")
@@ -7,6 +8,10 @@ def homepage(request):
 def test(request):
     todo_list = ToDo.objects.all()
     return render(request, "test.html", {"todo_list": todo_list})
+
+def books(request):
+    books_list = Books.objects.all()
+    return render(request, "books.html", {"books_list": books_list})
 
 def second(request):
     return HttpResponse("Test page 2")
